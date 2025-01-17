24260c61de62a00c39c3ebbdf4dc43a87a626abf
Gabriel_Martins_Padoin <gabriel.padoin@woli.com.br>
2025-01-16 22:31:43 -0300

    [NG] ANGULARS_DEPENDENCY_INJECTION_MECHANISM - feat: using angular's dependency injection mechanism to inject TasksService onto the NewTaskComponent

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, inject, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TasksService } from '../tasks.service';
 // import { TasksService } from '../tasks.service';
 
 @Component({
@@ -11,15 +12,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
-  // private tasksService: TasksService;
 
-  constructor() {
-    // this.tasksService = new TasksService()
-    // this.tasksService = inject(TasksService)
-  }
+  constructor(private tasksService: TasksService) {}
 
   onAddTask(title: string, description: string) {
-    // this.tasksService.addTask({title, description})
+    this.tasksService.addTask({title, description})
     this.formEl()?.nativeElement.reset();
   }
 }
