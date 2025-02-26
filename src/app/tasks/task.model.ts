a802708a2c51339fad4192166dec2cff02eea7ee
gmartpad <contato.gabrielmartinspadoin@gmail.com>
2025-02-26 19:48:23 -0300

    [NG] PREPEARING_A_NON-CLASS_VALUE_FOR_INJECTION - feat: creating and preparing the TaskStatusOptions value in the task.model.ts file for injection where task status options are listed

--- src/app/tasks/task.model.ts
+++ src/app/tasks/task.model.ts
@@ -1,5 +1,29 @@
 export type TaskStatus = 'OPEN' | 'IN_PROGRESS' | 'DONE';
 
+export type TaskStatusOption = {
+  value: 'open' | 'in-progress' | 'done';
+  taskStatus: TaskStatus;
+  text: string;
+};
+
+export const TaskStatusOptions: TaskStatusOption[] = [
+  {
+    value: 'open',
+    taskStatus: 'OPEN',
+    text: 'Open',
+  },
+  {
+    value: 'in-progress',
+    taskStatus: 'IN_PROGRESS',
+    text: 'In-Progress',
+  },
+  {
+    value: 'done',
+    taskStatus: 'DONE',
+    text: 'Completed',
+  },
+];
+
 export interface Task {
   id: string;
   title: string;
